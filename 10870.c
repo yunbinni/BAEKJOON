a461dedea4ceef07dc849b48d7584e6aae149808
yunbinni <yunbin9049@gmail.com>
2021-06-10 01:24:11 +0900

    10870 added

---
+++ 10870.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int f(int n) {
+	if(n == 0) return 0;
+	else if(n == 1) return 1;
+	else return f(n - 2) + f(n - 1);
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	
+	printf("%d", f(n));
+	
+	return 0;
+}
